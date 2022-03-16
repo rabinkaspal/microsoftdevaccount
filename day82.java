a2ae25aa50ed339d1a1590eeb7bbbd84f1d4387c
Rabin Kaspal <rabinkaspal@gmail.com>
2022-03-17 04:57:02 +1100

    Update day82.java

--- day82.java
+++ day82.java
@@ -3,3 +3,13 @@
 public static void main(args[]){
   System.out.println("day 82");
 }
+
+
+public static void main(args[]){
+  System.out.println("day 82");
+}
+
+
+public static void main(args[]){
+  System.out.println("day 82");
+}
